306e8052753158d738c63775cade0404e3401cb6
Alagris <alagris12358@gmail.com>
2021-12-27 14:08:31 +0100

    better predictive coding

--- predictive_coding_sparse_sep_plus_epsilon.py
+++ predictive_coding_sparse_sep_plus_epsilon.py
@@ -13,8 +13,10 @@ MNIST, LABELS = torch.load('htm/data/mnist.pt')
 PATCH_SIZE = np.array([5, 5])
 POP_SIZE2 = 20
 POP_SIZE1 = 80
-
-w1 = np.random.rand(PATCH_SIZE.prod(), POP_SIZE1)
+sep_sparsity = 5
+w1 = np.zeros((PATCH_SIZE.prod(), POP_SIZE1))
+for i in range(POP_SIZE1):
+    w1[np.random.permutation(PATCH_SIZE.prod())[:sep_sparsity], i] = 1
 w2 = np.random.rand(POP_SIZE1, POP_SIZE2)
 w1 = w1 / w1.sum(0)
 w2n = w2 / w2.sum(0)
@@ -27,48 +29,46 @@ def rand_patch():
     left_bottom = (img.shape - PATCH_SIZE) * r
     left_bottom = left_bottom.astype(int)
     top_right = left_bottom + PATCH_SIZE
-    return img[left_bottom[0]:top_right[0], left_bottom[1]:top_right[1]]
+    img = img[left_bottom[0]:top_right[0], left_bottom[1]:top_right[1]]
+    return np.float32(img / 255 > 0.8)
 
 
 threshold = 0.2
-epsilon = 0.0001
+epsilon = 0.001
 
 
 def run(x):
-    x1 = np.float32(x / 255 > 0.8)
-    s1 = x1.reshape(-1) @ w1
+    s1 = x.reshape(-1) @ w1
     top_k = np.argpartition(s1, -k1)[-k1:]
     x2 = np.zeros(POP_SIZE1)
     x2[top_k] = 1
     s2 = x2 @ w2n
     top_1 = s2.argmax()
-    return x1, x2, top_1
+    return x2, top_1
 
 
 def learn(x2, top_1):
     global w2n
-    w2[:, top_1] += epsilon * (x2 - 0.5) * 2
+    w2[:, top_1] = epsilon * x2 + w2[:, top_1]
     w2n = w2 / w2.sum(0)
 
 
-for _ in tqdm(range(1000), desc="training"):
-    img = rand_patch()
-    if img.sum() == 0:
-        continue
-    _, middle, top = run(img)
-    learn(middle, top)
-
-stats = np.zeros((POP_SIZE2, PATCH_SIZE[0], PATCH_SIZE[1]))
-for _ in tqdm(range(500), desc="eval"):
-    img = rand_patch()
-    if img.sum() == 0:
-        continue
-    bottom, _, top = run(img)
-    stats[top] += bottom
-
 w, h = 5, 4
 fig, axs = plt.subplots(w, h)
-for i in range(w):
-    for j in range(h):
-        axs[i, j].imshow(stats[i + j * w])
+test_patches = [rand_patch() for _ in range(20000)]
+for idx in tqdm(range(200000), desc="training"):
+    img = rand_patch()
+    middle, top = run(img)
+    learn(middle, top)
+    if idx % 2000 == 0:
+        stats = np.zeros((POP_SIZE2, PATCH_SIZE[0], PATCH_SIZE[1]))
+        for img in tqdm(test_patches, desc="eval"):
+            _, top = run(img)
+            stats[top] += img
+
+        for i in range(w):
+            for j in range(h):
+                axs[i, j].imshow(stats[i + j * w])
+        plt.pause(0.01)
+
 plt.show()
